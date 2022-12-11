81a0fe3454dcfe464c6a56ce56856f25e6e7855b
fabiooo4 <fabioirimie@gmail.com>
2022-12-11 22:27:56 +0100

    made cards clickable and added a popup for every pokemon

--- src/routes/view/PokemonList.svelte
+++ src/routes/view/PokemonList.svelte
@@ -4,30 +4,47 @@
 
   // Numbers of pokemon to fetch
   //! It must be a mlutiple of 3
-  let limit = 102;
+  let limit = 153;
 
   let promise = fetchPokemon(limit);
 </script>
 
+<!-- Grid -->
 <div class="flex justify-center w-4/5 p-8">
   <div class="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
     {#await promise}
-      <div class="flex flex-col items-center justify-center">
+      <!-- Loading spinner -->
+      <div class="flex flex-col items-center justify-center col-span-3">
         <div class="flex flex-row items-center justify-center">
           <div class="w-12 h-12 border-dotted border-1 border-t-4 border-indigo-900 rounded-full animate-spin"></div>
         </div>
       </div>
     {:then}
       {#each pokemonList as pokemon}
+        <!-- Pokemon card -->
         <div class="card group bg-base-100 shadow-xl m-4 transition ease-in-out hover:bg-base-300 active:scale-95 duration-150">
-          <div class="absolute justify-start p-4 z-0">
+          <a href="#{pokemon.name.toLowerCase()}"> <!-- Link to pokemon popup page -->
+          <div class="absolute justify-start p-4 z-20">
             <h1 class="font-black text-6xl lg:text-6xl md:text-6xl sm:text-red text-gray-200 group-hover:text-gray-100">#{pokemon.id}</h1>
           </div>
-          <figure class="px-10 pt-10 z-10">
+
+          <figure class="px-10 pt-10 z-30">
             <img src={pokemon.sprite} alt={pokemon.name} class="rounded-xl" />
           </figure>
-          <div class="card-body items-center text-center">
+
+          <div class="card-body items-center text-center z-30">
             <h2 class="card-title font-black text-3xl">{pokemon.name}</h2>
+          </div>
+          </a>
+        </div>
+        
+        <!-- Pokemon popup page -->
+        <div class="modal" id="{pokemon.name.toLowerCase()}">
+          <div class="modal-box">
+            <h3 class="font-bold text-lg">{pokemon.id}  {pokemon.name}</h3>
+            <div class="modal-action">
+            <a href="/#" class="btn">Yay!</a>
+            </div>
           </div>
         </div>
       {/each}
