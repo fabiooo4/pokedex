76cdfb6541d5856823c57242906a8adf7ff012ad
fabiooo4 <fabioirimie@gmail.com>
2022-12-14 19:25:54 +0100

    fixed evolution chain

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="flex flex-col items-center justify-center">
-  <h1 class="font-extrabold text-9xl m-5">Pokédex</h1>
+  <h1 class="font-extrabold text-7xl lg:text-9xl md:text-8xl sm:text-7xl m-5">Pokédex</h1>
   <PokemonList />
 </div>
--- src/routes/view/PokemonList.svelte
+++ src/routes/view/PokemonList.svelte
@@ -55,7 +55,7 @@
         
         <!-- Pokemon popup page -->
         <div class="modal" id="{pokemon.name.toLowerCase()}">
-          <div class="modal-box [&::-webkit-scrollbar]:hidden">
+          <div class="modal-box [&::-webkit-scrollbar]:hidden overflow-x-hidden">
 
             <!-- Name -->
             <h3 class="flex justify-center font-extrabold text-6xl p-0">{pokemon.name}</h3>
@@ -91,7 +91,7 @@
                   <!-- check if hidden and display in different columns-->
                   {#if ability.is_hidden}
 
-                    <div class="group cursor-pointer relative inline-block w-24 text-center">
+                    <div class="group cursor-pointer relative inline-block w-24 text-center mr-4">
                       <!-- Ability name -->
                       <span class="inline-block w-max justify-center bg-gray-200 text-black rounded-full px-3 py-1 mt-2 text-sm font-semibold">{ability.name.charAt(0).toUpperCase() + ability.name.slice(1)}
                       
@@ -100,6 +100,7 @@
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
                       </span>
+                      
 
                       <div class="opacity-0 transition-opacity duration-300 w-52 bg-black text-white text-center text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full -left-1/2 px-3 pointer-events-none">
                         
@@ -112,7 +113,7 @@
 
                   {:else}
                   
-                    <div class="group cursor-pointer relative inline-block w-24 text-center">
+                    <div class="group cursor-pointer relative inline-block w-24 text-center mr-4">
                       <!-- Ability name -->
                       <span class="inline-block w-max justify-center bg-gray-200 text-black rounded-full px-3 py-1 mt-2 text-sm font-semibold">{ability.name.charAt(0).toUpperCase() + ability.name.slice(1)}
                       
@@ -139,15 +140,27 @@
             <!-- TODO  Stats-->
 
             <!-- Evolution chain -->
-            <h1 class="flex justify-center font-extrabold text-xl p-2">Evolution chain</h1>
-            <div class="flex">
-              <div class="grid gap-x-10 grid-rows-5 grid-cols-{pokemon.evolutionChain.length} grid-flow-col">
-                {#each pokemon.evolutionChain as evolution}
-                  <img src={evolution.sprite} alt={evolution.name} class="bg-gray-200 shadow-lg p-1 rounded-xl row-span-4"/>
-                  <h1 class="text-center font-bold text-lg">{evolution.name}</h1>
-                {/each}
+            {#if pokemon.evolutionChain.length >= 3}
+              <h1 class="flex justify-center font-extrabold text-xl p-2">Evolution chain</h1>
+              <div class="flex">
+                <div class="grid gap-x-8 lg:gap-x-10 md:gap-x-10 sm:gap-x-8 grid-rows-5 grid-cols-{pokemon.evolutionChain.length} grid-flow-col">
+                  {#each pokemon.evolutionChain as evolution}
+                    <img src={evolution.sprite} alt={evolution.name} class="bg-gray-200 shadow-lg p-1 rounded-xl row-span-4"/>
+                    <h1 class="text-center font-bold text-lg row-span-1">{evolution.name}</h1>
+                  {/each}
+                </div>
               </div>
-            </div>
+            {:else if pokemon.evolutionChain.length ==2}
+              <h1 class="flex justify-center font-extrabold text-xl p-2">Evolution chain</h1>
+              <div class="flex flex-nowrap">
+                <div class="grid gap-x-10 mx-10 lg:mx-[84px] md:mx-[84px] sm:mx-10 shrink grid-rows-5 grid-cols-{pokemon.evolutionChain.length} grid-flow-col">
+                  {#each pokemon.evolutionChain as evolution}
+                    <img src={evolution.sprite} alt={evolution.name} class="bg-gray-200 shadow-lg p-1 rounded-xl row-span-4"/>
+                    <h1 class="text-center font-bold text-md row-span-1">{evolution.name}</h1>
+                  {/each}
+                </div>
+              </div>
+            {/if}
             
             <!-- TODO: Close button fixed to page OR close modal by clicking away -->
             <div class="modal-action">
